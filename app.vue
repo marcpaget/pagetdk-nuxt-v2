43b58b6a202485d880cf83463909fb8e35737961
marcpaget <marc.d.paget@gmail.com>
2025-02-08 04:20:47 +0100

    added 3 pages from branch

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+        <NuxtLoadingIndicator />
   <HorizontalNavBar />
   <NuxtPage />
 </template>
---
+++ pages/srilankatimeline.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+      <Galleria :value="images" :responsiveOptions="responsiveOptions" :numVisible="5" :circular="true" containerStyle="max-width: 640px" :showItemNavigators="true" :showItemNavigatorsOnHover="true">
+          <template #item="slotProps">
+              <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block" />
+          </template>
+          <template #thumbnail="slotProps">
+              <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" style="display: block" />
+          </template>
+      </Galleria>
+  </div>
+</template>
+
+
+<script setup>
+
+</script>
